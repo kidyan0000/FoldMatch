f1a5cf3ab258b37d8d75e1442d591ebeb72290eb
kidyan0000 <756668972@qq.com>
2018-12-12 16:27:06 +0100

    add question 20181012

---
+++ build/CMakeFiles/CLOTH_SIMULATION.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/src/ClothMain.cpp" "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/build/CMakeFiles/CLOTH_SIMULATION.dir/src/ClothMain.cpp.o"
+  "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/src/clothwindow.cpp" "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/build/CMakeFiles/CLOTH_SIMULATION.dir/src/clothwindow.cpp.o"
+  "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/src/main.cpp" "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/build/CMakeFiles/CLOTH_SIMULATION.dir/src/main.cpp.o"
+  "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/src/ply_Module.cpp" "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/build/CMakeFiles/CLOTH_SIMULATION.dir/src/ply_Module.cpp.o"
+  "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/src/tinyply.cpp" "/home/kidyan0000/Dropbox/P.Thesis_Seamless_Cloth_Simulation_with_High_Geometric_Fidelity/Workspace/cloth_simulation/build/CMakeFiles/CLOTH_SIMULATION.dir/src/tinyply.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../headers"
+  "../data"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
