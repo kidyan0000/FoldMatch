265c42bfa7a8e90a30b6f0eced81262c3ed0dbbf
kidyan0000 <756668972@qq.com>
2019-01-15 09:01:59 +0100

    2019/01/15

--- src/cloth_calc.cpp
+++ src/cloth_calc.cpp
@@ -26,43 +26,44 @@ void cloth_calc::cloth_eig()
     }
 
     // initialize the matrix to store the vector values of each triangles
-    Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> VecR(faces.rows()*3,2);
-    Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> VecT(faces.rows()*3,2);
+    Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> VecR(faces.rows()*3,6);
+    Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> VecT(faces.rows()*3,6);
     /*
-     *Vec = [ index_e1_v1 index_e1_v2
-     *        index_e1_v1 index_e1_v3
-     *        index_e1_v2 index_e1_v3
-     *                                ]
+     *Vec = [
+     *        el1_vet1_v1_x el1_vet1_v1_y el1_vet1_v1_z el1_vet1_v2_x el1_vet1_v2_y el1_vet1_v2_z
+     *        el1_vet2_v1_x el1_vet2_v1_y el1_vet2_v1_z el1_vet2_v2_x el1_vet2_v2_y el1_vet2_v2_z
+     *        el1_vet3_v1_x el1_vet3_v1_y el1_vet3_v1_z el1_vet3_v2_x el1_vet3_v2_y el1_vet3_v2_z
+     *      ]
      */
 
+    // get the verts index of each triangles el=i
+    // faces(i,j) is the ith triangle of the vertex j
     // FOR REFERENCE
-    // get the verts index of each triangles i
-
     int k = 0;
     for(int i=0; i<faces.rows(); i++)
     {
-            VecR.row(k) <<   (vertsR(faces(i,0)) - vertsR(faces(i,1))), // vector at vertex 0
-                             (vertsR(faces(i,0)) - vertsR(faces(i,2)));
-            VecR.row(k+1) << (vertsR(faces(i,1)) - vertsR(faces(i,0))), // vector at vertex 1
-                             (vertsR(faces(i,1)) - vertsR(faces(i,2)));
-            VecR.row(k+2) << (vertsR(faces(i,2)) - vertsR(faces(i,0))), // vector at vertex 2
-                             (vertsR(faces(i,2)) - vertsR(faces(i,1)));
+            VecR.row(k) <<   (vertsR.row(faces(i,0)) - vertsR.row(faces(i,1))), // vector at vertex 0
+                             (vertsR.row(faces(i,0)) - vertsR.row(faces(i,2)));
+            VecR.row(k+1) << (vertsR.row(faces(i,1)) - vertsR.row(faces(i,0))), // vector at vertex 1
+                             (vertsR.row(faces(i,1)) - vertsR.row(faces(i,2)));
+            VecR.row(k+2) << (vertsR.row(faces(i,2)) - vertsR.row(faces(i,0))), // vector at vertex 2
+                             (vertsR.row(faces(i,2)) - vertsR.row(faces(i,1)));
             k = k+3;
     }
-
     // FOR TEMPLATE
     int j = 0;
     for(int i=0; i<faces.rows(); i++)
     {
-            VecT.row(j) <<   (vertsT(faces(i,0)) - vertsT(faces(i,1))), // vector at vertex 0
-                             (vertsT(faces(i,0)) - vertsT(faces(i,2)));
-            VecT.row(j+1) << (vertsT(faces(i,1)) - vertsT(faces(i,0))), // vector at vertex 1
-                             (vertsT(faces(i,1)) - vertsT(faces(i,2)));
-            VecT.row(j+2) << (vertsT(faces(i,2)) - vertsT(faces(i,0))), // vector at vertex 2
-                             (vertsT(faces(i,2)) - vertsT(faces(i,1)));
+            VecT.row(j) <<   (vertsT.row(faces(i,0)) - vertsT.row(faces(i,1))), // vector at vertex 0
+                             (vertsT.row(faces(i,0)) - vertsT.row(faces(i,2)));
+            VecT.row(j+1) << (vertsT.row(faces(i,1)) - vertsT.row(faces(i,0))), // vector at vertex 1
+                             (vertsT.row(faces(i,1)) - vertsT.row(faces(i,2)));
+            VecT.row(j+2) << (vertsT.row(faces(i,2)) - vertsT.row(faces(i,0))), // vector at vertex 2
+                             (vertsT.row(faces(i,2)) - vertsT.row(faces(i,1)));
             j = j+3;
     }
 
+    // this is for debug
 
     // initialize the matrix to store the eigenvalue and eigenvector
     Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> eigval(faces.rows()*3,2);
@@ -71,9 +72,10 @@ void cloth_calc::cloth_eig()
     int l = 0;
     for(int i=0; i<faces.rows()*3; i++)
     {
-        // compute the transformation matrix
+        // compute the transformation matrix (Transl)
         // T = [u1, u2] * [u1_, u2_]^-1
-        Eigen::MatrixXd Transl(Eigen::Map<Eigen::RowVectorXd>(VecT.row(i).data(), 2).transpose() * Eigen::Map<Eigen::RowVectorXd>(VecR.row(i).data(), 2));
+        // we have here 3D triangles, thus we need to transform at first to 2D triangles using projecting
+        Eigen::MatrixXd Transl((Eigen::Map<Eigen::Matrix<double,3,2> >(VecT.row(i).data())).block<2,2>(0,0) * ((Eigen::Map<Eigen::Matrix<double,3,2> >(VecR.row(i).data())).block<2,2>(0,0)).inverse());
 
         // compute the eigenvalues and eigenvectors of transformation matrix and save it to eigval and eigvec
         // U^2 = T^transpose * T
@@ -81,7 +83,11 @@ void cloth_calc::cloth_eig()
 
         eigval.row(l) << (solv.eigenvalues()).transpose();
         eigvec.row(l) << ((solv.eigenvectors()).col(0)).transpose(), ((solv.eigenvectors()).col(1)).transpose();
+        l = l+1;
     }
+    // this is for debug
+    std::cout << eigvec << std::endl;
+
 }
 
 void cloth_calc::cloth_defo()
