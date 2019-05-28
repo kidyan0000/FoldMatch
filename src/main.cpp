0686ab70f98c126a7445ec955b64301478d559d0
kidyan0000 <756668972@qq.com>
2019-05-28 22:29:14 +0200

    20190527

--- src/main.cpp
+++ src/main.cpp
@@ -689,7 +689,7 @@ int main(int argc, char *argv[])
     ////////////////////////////////
 
     // slot should be chosen from 1 to 74
-    for(int slot=2; slot<10; slot++)
+    for(int slot=2; slot<51; slot++)
     {
         CT = slot;
         CR = slot+1;
